be63723f81ae52fe3d55b6de3754f34eb89a65aa
Sung Yi <sy@Sungs-Air.fios-router.home>
2023-06-07 20:09:54 -0400

    AGAIN! One small step for man, one giant leap for DATAgit add . .

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-
+const { createArr } = require("./src/gen.js");
+const { writeJSON } = require("./src/fs.js");
 
 function run(){
     const argc = process.argv.length;
@@ -12,7 +13,7 @@ function run(){
         case "detail":
             break;
         case "faker":
-            let temp = process.argv[3] ? createArrF(Number(process.argv[3])) : createArrF(10);
+            let temp = process.argv[3] ? createArr(Number(process.argv[3])) : createArr(10);
             writeJSON(`./data`, 'data.json', temp);
             break;
         default:
